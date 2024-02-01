eaf750d25c015353d752f602027185cb78a5009f
Melody <137832068+Melody-Lisa@users.noreply.github.com>
2024-02-01 12:02:31 +0000

    POST

--- run.py
+++ run.py
@@ -30,9 +30,12 @@ def about_member(member_name):
     return render_template("member.html", member=member)
 
 
-@app.route('/contact')
+@app.route("/contact", methods=["GET", "POST"])
 def contact():
-    return render_template('contact.html', page_title='Contact')
+    if request.method == "POST":
+        print(request.form.get("name"))
+        print(request.form["email"])
+    return render_template("contact.html", page_title="Contact")
 
 
 @app.route('/careers')
