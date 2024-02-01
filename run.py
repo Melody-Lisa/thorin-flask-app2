1dd4cc62b35f91d4fc5a059419e7b8cb860e3e99
Melody <137832068+Melody-Lisa@users.noreply.github.com>
2024-02-01 13:32:31 +0000

    Providing Feedback To The User

--- run.py
+++ run.py
@@ -1,14 +1,17 @@
 import os
 import json
-from flask import Flask, render_template
+from flask import Flask, render_template, request, flash
+if os.path.exists("env.py"):
+    import env
 
 
 app = Flask(__name__)
+app.secret_key = os.environ.get("SECRET_KEY")
 
 
-@app.route('/')
+@app.route("/")
 def index():
-    return render_template('index.html')
+    return render_template("index.html")
 
 
 @app.route("/about")
@@ -33,18 +36,18 @@ def about_member(member_name):
 @app.route("/contact", methods=["GET", "POST"])
 def contact():
     if request.method == "POST":
-        print(request.form.get("name"))
-        print(request.form["email"])
+        flash("Thanks {}, we have received your message!".format(
+            request.form.get("name")))
     return render_template("contact.html", page_title="Contact")
 
 
-@app.route('/careers')
+@app.route("/careers")
 def careers():
-    return render_template('careers.html', page_title='Careers')
+    return render_template("careers.html", page_title="Careers")
 
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     app.run(
-        host=os.environ.get('IP', '0.0.0.0'),
-        port=int(os.environ.get('PORT', 5000)),
+        host=os.environ.get("IP", "0.0.0.0"),
+        port=int(os.environ.get("PORT", "5000")),
         debug=True)
